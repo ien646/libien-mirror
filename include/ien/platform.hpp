2c1e65d7bb8215fedd6b6cd6abbbbe252b8b3b30
ien <ien@amsostudios.com>
2022-01-06 00:00:00 +0000

    initial commit

---
+++ include/ien/platform.hpp
@@ -0,0 +1,207 @@
+#pragma once
+
+//--------------------------------------------
+// ARCH
+//--------------------------------------------
+#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_AMD64)
+    #define IEN_ARCH_X86_64
+#endif
+
+#if defined(i386) || defined(__i386) || defined(__i386__) || defined(_M_IX86)
+    #define IEN_ARCH_X86
+#endif
+
+#if defined(__arm__) || defined(_M_ARM)
+    #define IEN_ARCH_ARM
+#endif
+
+#if defined(__aarch64__)
+    #define IEN_ARCH_ARM64
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// OS
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#if defined(_WIN64)
+    #define IEN_OS_WIN64
+#elif defined(_WIN32)
+    #define IEN_OS_WIN32
+#endif
+
+#if defined(__unix__)
+    #define IEN_OS_UNIX
+#endif
+
+#if defined(__APPLE__)
+    #define IEN_OS_MAC
+#endif
+
+#if defined(__linux__)
+    #define IEN_OS_LINUX
+#endif
+
+#if defined(__FreeBSD__)
+    #define IEN_OS_FREEBSD
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// COMPILER
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#if defined(_MSC_VER)
+    #define IEN_COMPILER_MSVC
+    #define IEN_COMPILER_MSVC_VER _MSC_VER
+
+#elif defined(__GNUC__) && !defined(__clang__)
+    #define IEN_COMPILER_GNU
+    #define IEN_COMPILER_GNU_VER __GNUC__
+
+#elif defined(__clang__)
+    #define IEN_COMPILER_CLANG
+    #define IEN_COMPILER_CLANG_VER __clang_major__
+
+#elif defined(__INTEL_COMPILER)
+    #define IEN_COMPILER_INTEL
+    #define IEN_COMPILER_INTEL_VER __INTEL_COMPILER
+
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// ARM SPECIFIC
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#if defined(IEN_ARCH_ARM) || defined(IEN_ARCH_ARM64)
+    #if defined(__ARM_NEON) || defined(__ARM_NEON__)
+        #ifndef IEN_ARM_NEON
+            #define IEN_ARM_NEON
+        #endif
+    #endif
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// ALIGNMENT
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#include <cstddef>
+
+namespace ien
+{
+    template<typename T>
+    constexpr bool is_ptr_aligned(const T* ptr, size_t alignment)
+    {
+        auto ptrval =
+            reinterpret_cast<const char*>(ptr) -
+            reinterpret_cast<const char*>(0);
+
+        return (ptrval % alignment) == 0;
+    }
+}
+
+#define IEN_ALIGNED_SZ(sz, alig) (sz - (sz % alig) + alig)
+
+#if defined(IEN_ARCH_X86_64) || defined(IEN_ARCH_X86)
+    #define IEN_DEFAULT_ALIGNMENT 32
+#elif defined(IEN_ARCH_ARM) || defined(IEN_ARCH_ARM64)
+    #define IEN_DEFAULT_ALIGNMENT 16
+#else
+    #define IEN_DEFAULT_ALIGNMENT 32
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// COMPILER HINTS
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+// -- Unreachable case --
+#if defined(IEN_COMPILER_MSVC)
+    #define IEN_HINT_UNREACHABLE() __assume(false)
+#elif defined(IEN_COMPILER_GNU) || defined(IEN_COMPILER_CLANG) || defined(IEN_COMPILER_INTEL)
+    #define IEN_HINT_UNREACHABLE() __builtin_unreachable()
+#else
+    #define IEN_HINT_UNREACHABLE()
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// C++ VERSION
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#if __cplusplus >= 199711L
+    #define IEN_HAS_CPP03
+#endif
+#if __cplusplus >= 201103L
+    #define IEN_HAS_CPP11
+#endif
+#if __cplusplus >= 201402L
+    #define IEN_HAS_CPP14
+#endif
+#if __cplusplus >= 201703L
+    #define IEN_HAS_CPP17
+#endif
+#if __cplusplus >= 202002L
+    #define IEN_HAS_CPP20
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// UNSUPPORTED COMPILER GUARDS
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#ifndef IEN_COMPILER_IGNORE_VER
+    #if defined(IEN_COMPILER_MSVC)
+        #if IEN_COMPILER_MSVC_VER < 1900
+            #error "Unsupported Microsoft Visual C++ Compiler Version!"
+            #error "A compiler with at least C++17 support is required"
+            #error "If you believe this error to be a false positive, define IEN_COMPILER_IGNORE_VER in your code"
+        #endif
+    #elif defined(IEN_COMPILER_GNU)
+        #if IEN_COMPILER_GNU_VER < 7
+            #error "Unsupported GNU C++ Compiler Version!"
+            #error "A compiler with at least C++17 support is required"
+            #error "If you believe this error to be a false positive, define IEN_COMPILER_IGNORE_VER in your code"
+        #endif
+    #elif defined(IEN_COMPILER_CLANG)
+        #if IEN_COMPILER_CLANG_VER < 5
+            #error "Unsupported Clang C++ Compiler Version!"
+            #error "A compiler with at least C++17 support is required"
+            #error "If you believe this error to be a false positive, define IEN_COMPILER_IGNORE_VER in your code"
+        #endif
+    #elif defined(IEN_COMPILER_INTEL)
+        #if IEN_COMPILER_INTEL_VER < 1900
+            #error "Unsupported Intel C++ Compiler Version!"
+            #error "A compiler with at least C++17 support is required"
+            #error "If you believe this error to be a false positive, define IEN_COMPILER_IGNORE_VER in your code"
+        #endif
+    #endif
+#endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// PLATFORM FEATURES (x86)
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#if defined(IEN_ARCH_X86) || defined(IEN_ARCH_X86_64)
+
+#include <iostream>
+namespace ien::platform::x86
+{
+    enum class feature : int
+    {
+        ABM, ADX,
+        AES, AVX2,
+        AVX512BW, AVX512CD,
+        AVX512DQ, AVX512ER,
+        AVX512F, AVX512IFMA,
+        AVX512PF, AVX512VBMI,
+        AVX512VL, AVX,
+        BMI1, BMI2,
+        FMA3, FMA4,
+        MMX, PREFETCHWT1,
+        RDRAND, SHA,
+        SSE2, SSE3,
+        SSE41, SSE42,
+        SSE4a, SSE,
+        SSSE3, x64,
+        XOP
+    };
+
+    extern void force_feature(feature, bool);
+    extern bool get_feature(feature);
+    extern void print_enabled_features(std::ostream& = std::cout);
+}
+
+#endif
