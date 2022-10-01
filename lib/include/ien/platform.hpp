67ca28a60bddee254c180e84abe629156340ee00
ien <ien@amsostudios.com>
2022-10-01 00:00:00 +0000

    enable/disable warnings macros

--- lib/include/ien/platform.hpp
+++ lib/include/ien/platform.hpp
@@ -180,3 +180,17 @@ namespace ien::platform::x86
 }
 
 #endif
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// WARNINGS
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\
+
+#ifdef IEN_COMPILER_MSVC
+    #define IEN_WARNINGS_DISABLE_BEGIN _Pragma("warning(push, 0)")
+    #define IEN_WARNINGS_DISABLE_END _Pragma("warning(pop)")
+#else
+    #define IEN_WARNINGS_DISABLE_BEGIN \
+        _Pragma("GCC diagnostic push") \
+        _Pragma("GCC diagnostic ignored \"-Wall\"")
+    #define IEN_WARNINGS_DISABLE_END _Pragma("GCC diagnostic pop")
+#endif
