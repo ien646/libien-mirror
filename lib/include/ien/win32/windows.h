0ab360cddbd01a8efe2e7157078f1d0dd44e1d11
ien <ien@amsostudios.com>
2022-10-08 00:00:00 +0000

    fix windows includes and warnings

---
+++ lib/include/ien/win32/windows.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ien/platform.hpp>
+
+#ifndef IEN_OS_WIN
+	#error "Unable to use Windows headers outside of Windows"
+#endif
+
+#define NOMINMAX
+#define WIN32_NO_STATUS
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#undef NOMINMAX
+#undef WIN32_NO_STATUS
+#undef WIN32_LEAN_AND_MEAN
