57e8ed15b24a241506e53564abe30bde27b82218
ien <ien@amsostudios.com>
2022-10-09 00:00:00 +0000

    code organization

---
+++ lib/include/ien/bits/str_utils/common.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ien/lang_utils.hpp>
+#include <ien/platform.hpp>
+
+namespace ien::detail
+{
+    template<concepts::AnyStrOrChar T, concepts::AnyStrOrChar ... TOther>
+    constexpr bool is_same_underlying_char_type =
+        (std::is_same_v<underlying_char_t<T>, underlying_char_t<TOther>> && ...);
+
+    template<concepts::AnyStr T>
+    constexpr std::basic_string_view<underlying_char_t<T>> str_view_range(const T& str, size_t offset, size_t maxlen)
+    {
+        using char_type = underlying_char_t<T>;
+        return std::basic_string_view<char_type>(
+            anystr_data(str) + offset,
+            std::min(anystr_length(str) - offset, maxlen)
+        );
+    }
+}
