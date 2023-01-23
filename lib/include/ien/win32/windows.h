9f097a585718c3e846a595e74e0a6e3d8df9e88d
ien <ien@amsostudios.com>
2023-01-23 00:00:00 +0000

    get_cmdline_wargs

--- lib/include/ien/win32/windows.h
+++ lib/include/ien/win32/windows.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <ien/platform.hpp>
+#include <string>
+#include <vector>
 
 #ifndef IEN_OS_WIN
 	#error "Unable to use Windows headers outside of Windows"
@@ -13,3 +15,8 @@
 #undef NOMINMAX
 #undef WIN32_NO_STATUS
 #undef WIN32_LEAN_AND_MEAN
+
+namespace win32
+{
+	extern std::vector<std::wstring> get_cmdline_wargs();
+}
