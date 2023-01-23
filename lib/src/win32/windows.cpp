0fa67abe39cee9718ff7b678440551715806c9b7
ien646 <1340246+ien646@users.noreply.github.com>
2023-01-23 00:00:00 +0000

    get_cmdline_wargs

---
+++ lib/src/win32/windows.cpp
@@ -0,0 +1,29 @@
+#include <ien/platform.hpp>
+#include <string>
+
+#ifdef IEN_OS_WIN
+
+#include <ien/win32/windows.h>
+#include <filesystem>
+#include <shellapi.h>
+
+namespace win32
+{
+    std::vector<std::wstring> get_cmdline_wargs()
+    {
+        std::vector<std::wstring> result;
+
+        int w_argc = 0;
+        auto cmdline = GetCommandLineW();
+        LPWSTR* w_argv = CommandLineToArgvW(cmdline, &w_argc);
+
+        for(int i = 0; i < w_argc; ++i)
+        {
+            result.push_back(std::wstring(w_argv[i]));
+        }
+        LocalFree(w_argv);
+        return result;
+    }
+}
+
+#endif
