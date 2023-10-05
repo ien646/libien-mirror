34be3f0b91e6f11bd7b8a9e5b75b1af3d90a282c
ien <ien@amsostudios.com>
2023-10-05 00:00:00 +0000

    fixes, error info

--- lib/include/ien/sftp/client.hpp
+++ lib/include/ien/sftp/client.hpp
@@ -18,34 +18,36 @@ namespace ien::sftp
         client_params _params = {};
         std::string _address;
         socket_t _socket = 0;
-        LIBSSH2_SESSION *_ssh_session = nullptr;
-        LIBSSH2_SFTP *_sftp_session = nullptr;
+        LIBSSH2_SESSION* _ssh_session = nullptr;
+        LIBSSH2_SFTP* _sftp_session = nullptr;
 #ifdef IEN_OS_WIN
-        WSADATA *_wsadata = nullptr;
+        WSADATA* _wsadata = nullptr;
 #endif
 
     public:
         explicit client(client_params params);
         ~client();
 
-        [[nodiscard]] bool file_exists(const std::string &remote_path) const;
-        [[nodiscard]] bool directory_exists(const std::string &remote_path) const;
+        [[nodiscard]] bool file_exists(const std::string& remote_path) const;
+        [[nodiscard]] bool directory_exists(const std::string& remote_path) const;
 
-        [[nodiscard]] bool is_temp_file(const file_entry &entry) const;
+        [[nodiscard]] bool is_temp_file(const file_entry& entry) const;
 
-        [[nodiscard]] directory_listing list_directory(const std::string &remote_path) const;
+        [[nodiscard]] directory_listing list_directory(const std::string& remote_path) const;
 
-        void get_file(const std::string &remote_path, const std::string &local_path) const;
-        void put_file(const std::string &local_path, const std::string &remote_path) const;
+        void get_file(const std::string& remote_path, const std::string& local_path) const;
+        void put_file(const std::string& local_path, const std::string& remote_path) const;
 
-        [[nodiscard]] bool create_directory(const std::string &path) const;
+        void create_directory(const std::string& path) const;
 
-        void clean_temp_files(const directory_listing &) const;
+        void clean_temp_files(const directory_listing&) const;
 
-        [[nodiscard]] file_info get_file_info(const std::string &remote_path) const;
+        [[nodiscard]] file_info get_file_info(const std::string& remote_path) const;
 
-        void set_atime(const std::string &remote_path, unsigned long mtime) const;
-        void set_mtime(const std::string &remote_path, unsigned long mtime) const;
+        void remove_file(const std::string& remote_path) const;
+
+        void set_atime(const std::string& remote_path, unsigned long mtime) const;
+        void set_mtime(const std::string& remote_path, unsigned long mtime) const;
 
     private:
         void init_address();
