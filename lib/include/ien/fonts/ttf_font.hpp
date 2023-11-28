1563a524118dba4ccff5e504af5efb4315d42497
ien <ien@amsostudios.com>
2023-11-28 00:00:00 +0000

    ttf font char info retrieval

--- lib/include/ien/fonts/ttf_font.hpp
+++ lib/include/ien/fonts/ttf_font.hpp
@@ -7,20 +7,30 @@
 
 namespace ien
 {
+    struct ttf_font_char_info
+    {
+        struct
+        {
+            unsigned short x0, y0, x1, y1;
+        } position;
+
+        struct
+        {
+            float x, y;
+        } offset;
+        
+        float x_advance;
+    };
     class ttf_font
     {
     private:
-        struct baked_char
-        {
-            unsigned short x0, y0, x1, y1;
-            float xoff, yoff, xadvance;
-        };
         std::unique_ptr<image> _atlas;
-        std::vector<baked_char> _char_info;
+        std::vector<ttf_font_char_info> _char_info;
 
     public:
         ttf_font(const std::string& path, float font_height_px, uint32_t atlas_size = 512, uint32_t num_chars = 256);
 
         inline const image& get_atlas() const { return *_atlas; }
+        const ttf_font_char_info& get_char_info(uint32_t ch) const;
     };
 } // namespace ien
