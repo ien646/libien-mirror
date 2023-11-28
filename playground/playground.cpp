75b9672fc58122a8ea381728a99633ccad0c5ef6
ien646 <1340246+ien646@users.noreply.github.com>
2023-11-28 00:00:00 +0000

    ttf font initial support

---
+++ playground/playground.cpp
@@ -0,0 +1,7 @@
+#include <ien/fonts/ttf_font.hpp>
+
+int main()
+{
+    ien::ttf_font font("/usr/local/share/fonts/c/cft.ttf", 55);
+    return 0;
+}
--- third-party/stb/src/stb.cpp
+++ third-party/stb/src/stb.cpp
@@ -1,4 +1,3 @@
-#include <type_traits>
 #ifdef _MSC_VER
     #define IEN_WARNINGS_DISABLE_BEGIN _Pragma("warning(push, 0)")
     #define IEN_WARNINGS_DISABLE_END _Pragma("warning(pop)")
@@ -7,15 +6,25 @@
     #define IEN_WARNINGS_DISABLE_END _Pragma("GCC diagnostic pop")
 #endif
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+IEN_WARNINGS_DISABLE_BEGIN
+
+#ifdef LIBIEN_STB_ENABLE_IMAGE
+    #define STB_IMAGE_IMPLEMENTATION
+    #include "../include/stb_image.h"
+
+    #define STB_IMAGE_RESIZE_IMPLEMENTATION
+    #include "../include/stb_image_resize.h"
+
+    #define STB_IMAGE_WRITE_IMPLEMENTATION
+    #include "../include/stb_image_write.h"
+#endif
+
+#ifdef LIBIEN_STB_ENABLE_FONTS
+    #define STB_TRUETYPE_IMPLEMENTATION
+    #include "../include/stb_truetype.h"
+#endif
 
 #define STBI_WINDOWS_UTF8
 #define STBIW_WINDOWS_UTF8
 
-IEN_WARNINGS_DISABLE_BEGIN
-#include "../include/stb_image.h"
-#include "../include/stb_image_resize.h"
-#include "../include/stb_image_write.h"
 IEN_WARNINGS_DISABLE_END
