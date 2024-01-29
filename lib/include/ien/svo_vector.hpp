d757470e22d956284a84825dc3387a6f49aab08f
ien <ien@amsostudios.com>
2024-01-29 00:00:00 +0000

    reduce svo_vector footprint

--- lib/include/ien/svo_vector.hpp
+++ lib/include/ien/svo_vector.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <ien/alloc.hpp>
 #include <ien/platform.hpp>
 
 #include <cassert>
+#include <cstring>
 #include <variant>
 #include <vector>
 
@@ -14,30 +16,40 @@ namespace ien
     private:
         static constexpr size_t max_stack_elems = MaxStackBytes / sizeof(T);
 
+        using stack_t = std::array<T, max_stack_elems>;
+        using heap_t = std::vector<T>;
+
         uint8_t _stack_size = 0;
-        std::array<T, max_stack_elems> _stack_data;
-        std::vector<T> _heap_data;
+        std::variant<stack_t, heap_t> _data = stack_t{};
 
     public:
+        using iterator = T*;
+        using const_iterator = const T*;
+
         void push_back(const T& elem)
         {
             if (!is_heap_allocated())
             {
                 if (_stack_size == max_stack_elems)
                 {
-                    _heap_data = std::vector<T>(max_stack_elems + 1);
-                    std::copy(_stack_data.begin(), _stack_data.end(), _heap_data.begin());
-                    _heap_data.back() = elem;
+                    const size_t bytes = max_stack_elems * sizeof(T);
+                    void* tempbuff = IEN_STACK_ALLOC(bytes);
+                    std::memcpy(tempbuff, get_stack_data().data(), bytes);
+                    _data = std::vector<T>(max_stack_elems + 1);
+
+                    auto& heap_data = get_heap_data();
+                    std:memcpy(heap_data.data(), tempbuff, bytes);
+                    heap_data.back() = elem;
                 }
                 else
                 {
-                    _stack_data[_stack_size] = elem;
+                    get_stack_data()[_stack_size] = elem;
                     ++_stack_size;
                 }
             }
             else
             {
-                _heap_data.push_back(elem);
+                get_heap_data().push_back(elem);
             }
         }
 
@@ -47,19 +59,24 @@ namespace ien
             {
                 if (_stack_size == max_stack_elems)
                 {
-                    _heap_data = std::vector<T>(max_stack_elems + 1);
-                    std::copy(_stack_data.begin(), _stack_data.end(), _heap_data.begin());
-                    _heap_data.back() = std::move(elem);
+                    const size_t bytes = max_stack_elems * sizeof(T);
+                    void* tempbuff = IEN_STACK_ALLOC(bytes);
+                    std::memcpy(tempbuff, get_stack_data().data(), bytes);
+                    _data = std::vector<T>(max_stack_elems + 1);
+
+                    auto& heap_data = get_heap_data();
+                    std:memcpy(heap_data.data(), tempbuff, bytes);
+                    heap_data.back() = std::move(elem);
                 }
                 else
                 {
-                    _stack_data[_stack_size] = std::move(elem);
+                    get_stack_data()[_stack_size] = std::move(elem);
                     ++_stack_size;
                 }
             }
             else
             {
-                _heap_data.push_back(elem);
+                get_heap_data().push_back(std::move(elem));
             }
         }
 
@@ -67,13 +84,13 @@ namespace ien
         {
             if (is_heap_allocated())
             {
-                assert(i < _heap_data.size());
-                return _heap_data[i];
+                assert(i < get_heap_data().size());
+                return get_heap_data()[i];
             }
             else
             {
-                assert(i < _stack_data.size());
-                return _stack_data[i];
+                assert(i < get_stack_data().size());
+                return get_stack_data()[i];
             }
         }
 
@@ -81,13 +98,13 @@ namespace ien
         {
             if (is_heap_allocated())
             {
-                assert(i < _heap_data.size());
-                return _heap_data[i];
+                assert(i < get_heap_data().size());
+                return get_heap_data()[i];
             }
             else
             {
-                assert(i < _stack_data.size());
-                return _stack_data[i];
+                assert(i < get_stack_data().size());
+                return get_stack_data()[i];
             }
         }
 
@@ -95,7 +112,7 @@ namespace ien
         {
             if (is_heap_allocated())
             {
-                return _heap_data.size();
+                return get_heap_data().size();
             }
             else
             {
@@ -107,39 +124,71 @@ namespace ien
         {
             if (!is_heap_allocated())
             {
-                return { _stack_data.begin(), _stack_data.end() };
+                return { get_stack_data().begin(), get_stack_data().end() };
             }
             else
             {
-                const auto result = std::move(_heap_data);
+                const auto result = std::move(get_heap_data());
                 return result;
             }
         }
 
-        bool is_heap_allocated() const { return !_heap_data.empty(); }
+        inline bool is_heap_allocated() const { return std::holds_alternative<heap_t>(_data); }
 
         T* data()
         {
-            if(is_heap_allocated())
+            if (is_heap_allocated())
             {
-                return _heap_data.data();
+                return get_heap_data().data();
             }
             else
             {
-                return _stack_data.data();
+                return get_stack_data().data();
             }
         }
 
         const T* data() const
         {
-            if(is_heap_allocated())
+            if (is_heap_allocated())
             {
-                return _heap_data.data();
+                return get_heap_data().data();
             }
             else
             {
-                return _stack_data.data();
+                return get_stack_data().data();
             }
         }
+
+        iterator begin() { return data(); }
+
+        const_iterator begin() const { return data(); }
+
+        iterator end()
+        {
+            if (is_heap_allocated())
+            {
+                return get_heap_data().data() + get_heap_data().size();
+            }
+            else
+            {
+                return get_stack_data().data() + _stack_size;
+            }
+        }
+
+        const iterator end() const
+        {
+            if (is_heap_allocated())
+            {
+                return get_heap_data().data() + get_heap_data().size();
+            }
+            else
+            {
+                return get_stack_data().data() + _stack_size;
+            }
+        }
+
+    private:
+        inline stack_t& get_stack_data() { return std::get<stack_t>(_data); }
+        inline heap_t& get_heap_data() { return std::get<heap_t>(_data); }
     };
 } // namespace ien
--- test/svo_vector.cpp
+++ test/svo_vector.cpp
@@ -4,9 +4,26 @@
 
 struct data_t
 {
-    int a,b,c;
+    int a, b, c;
     std::string x;
 };
+
+size_t alloc_count = 0;
+size_t alloc_bytes = 0;
+size_t dealloc_count = 0;
+
+void* operator new(size_t sz)
+{
+    ++alloc_count;
+    alloc_bytes += sz;
+    return malloc(sz);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    ++dealloc_count;
+    free(ptr);
+}
 
 TEST_CASE("SVO Vector")
 {
@@ -20,24 +37,93 @@ TEST_CASE("SVO Vector")
 
     SECTION("push_back()")
     {
+        const int initial_allocs = alloc_count;
+
         ien::svo_vector<int, sizeof(int) * 4> data;
         data.push_back(1);
         data.push_back(2);
         data.push_back(3);
         data.push_back(4);
 
+        REQUIRE(alloc_count == initial_allocs);
         REQUIRE(!data.is_heap_allocated());
         REQUIRE(data[0] == 1);
         REQUIRE(data[1] == 2);
         REQUIRE(data[2] == 3);
         REQUIRE(data[3] == 4);
-        
+
         data.push_back(5);
+        REQUIRE(alloc_count == initial_allocs + 1);
         REQUIRE(data.is_heap_allocated());
         REQUIRE(data[0] == 1);
         REQUIRE(data[1] == 2);
         REQUIRE(data[2] == 3);
         REQUIRE(data[3] == 4);
         REQUIRE(data[4] == 5);
+    };
+
+    SECTION("orphan_vector")
+    {
+        // Stack allocated svo_vector<T> gets coerced into heap allocated std::vector<T>
+        {
+            const int initial_allocs = alloc_count;
+
+            ien::svo_vector<int, sizeof(int) * 4> data;
+            data.push_back(1);
+            data.push_back(2);
+            data.push_back(3);
+            data.push_back(4);
+
+            REQUIRE(alloc_count == initial_allocs);
+            std::vector<int> orphan = data.orphan_vector();
+            REQUIRE(alloc_count == initial_allocs + 1);
+            REQUIRE(orphan[0] == 1);
+            REQUIRE(orphan[1] == 2);
+            REQUIRE(orphan[2] == 3);
+            REQUIRE(orphan[3] == 4);
+        }
+        {
+            const int initial_allocs = alloc_count;
+            ien::svo_vector<int, sizeof(int) * 4> data;
+            data.push_back(1);
+            data.push_back(2);
+            data.push_back(3);
+            data.push_back(4);
+            data.push_back(5);
+            REQUIRE(alloc_count == initial_allocs + 1);
+
+            std::vector<int> orphan = data.orphan_vector();
+            REQUIRE(alloc_count == initial_allocs + 1);
+            REQUIRE(orphan.size() == 5);
+            REQUIRE(orphan[0] == 1);
+            REQUIRE(orphan[1] == 2);
+            REQUIRE(orphan[2] == 3);
+            REQUIRE(orphan[3] == 4);
+            REQUIRE(orphan[4] == 5);
+        }
+    }
+
+    SECTION("begin()~end() iteration works (range-for loop)")
+    {
+        ien::svo_vector<int> data;
+        data.push_back(0);
+        data.push_back(1);
+        data.push_back(2);
+        data.push_back(3);
+
+        int i = 0;
+        for(const auto& e : data)
+        {
+            REQUIRE(e == i++);
+        }
+        REQUIRE(i == 4);
+        
+        data.push_back(4);
+        i = 0;
+        for(const auto& e : data)
+        {
+            REQUIRE(e == i++);
+        }
+        REQUIRE(i == 5);
     };
 };
