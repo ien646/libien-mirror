d256145928ea7d8edc2b880305fe3f2612acff54
ien <ien@amsostudios.com>
2024-05-10 00:00:00 +0000

    str_tolower/str_toupper

---
+++ lib/include/ien/bits/str_utils/tolowerupper.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <ien/lang_utils.hpp>
+#include <ien/bits/str_utils/common.hpp>
+
+namespace ien
+{
+    namespace detail
+    {
+        template<concepts::StdAnyStrStrV T, typename Op>
+        std::basic_string<ien::underlying_char_t<T>> str_tox(const T& str, Op op)
+        {
+            std::basic_string<ien::underlying_char_t<T>> result;
+            result.resize(str.size());
+            std::transform(str.cbegin(), str.cend(), result.begin(), op);
+            return result;
+        }
+    }
+
+    template<concepts::StdAnyStrStrV T>
+    std::basic_string<ien::underlying_char_t<T>> str_tolower(const T& str)
+    {
+        return detail::str_tox(str, std::tolower);
+    }
+
+    template<concepts::StdAnyStrStrV T>
+    std::basic_string<ien::underlying_char_t<T>> str_toupper(const T& str)
+    {
+        return detail::str_tox(str, std::toupper);
+    }
+}
