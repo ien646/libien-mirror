08e3921cc40fac95052f351640fed66ead2f02bc
ien646 <1340246+ien646@users.noreply.github.com>
2024-05-15 00:00:00 +0000

    roomba

--- lib/include/ien/math_utils.hpp
+++ lib/include/ien/math_utils.hpp
@@ -4,7 +4,7 @@
 
 namespace ien
 {
-    template <typename TValue, typename TRange0, typename TRange1, typename TRange2, typename TRange3>
+    template <concepts::Arithmetic TValue, concepts::Arithmetic TRange0, concepts::Arithmetic TRange1, concepts::Arithmetic TRange2, concepts::Arithmetic TRange3>
         requires(ien::all_convertible_to_v<TValue, TRange0, TRange1, TRange2, TRange3>)
     TValue remap(TValue value, TRange0 source_start, TRange1 source_end, TRange2 output_start, TRange3 output_end)
     {
