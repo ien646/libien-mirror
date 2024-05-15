debc990df65bee484a60d0ec9489e7954dfbc214
ien646 <1340246+ien646@users.noreply.github.com>
2024-05-15 00:00:00 +0000

    fix

--- lib/include/ien/container_utils.hpp
+++ lib/include/ien/container_utils.hpp
@@ -7,7 +7,9 @@ namespace ien
     template <concepts::SizedContainer TContainer, concepts::Integral TIndex>
     TIndex clamp_index_to_vector_bounds(const TContainer& container, const TIndex& index)
     {
-        TIndex result = std::min(index, container.size() - 1);
+        assert(container.size() < std::numeric_limits<TIndex>::max());
+
+        TIndex result = std::min(index, static_cast<TIndex>(container.size() - 1));
         if constexpr (std::is_signed_v<TIndex>)
         {
             result = std::max(static_cast<TIndex>(0), result);
