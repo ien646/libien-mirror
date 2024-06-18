8b28d75cc5c8729f4fd921cd340969dc82374159
ien646 <1340246+ien646@users.noreply.github.com>
2024-06-18 00:00:00 +0000

    fix ien::remap for integral types

--- lib/include/ien/math_utils.hpp
+++ lib/include/ien/math_utils.hpp
@@ -9,10 +9,17 @@ namespace ien
         concepts::Arithmetic TRange0,
         concepts::Arithmetic TRange1,
         concepts::Arithmetic TRange2,
-        concepts::Arithmetic TRange3>
+        concepts::Arithmetic TRange3,
+        concepts::FloatingPoint TIntermediate = double>
         requires(ien::all_convertible_to_v<TValue, TRange0, TRange1, TRange2, TRange3>)
     TValue remap(TValue value, TRange0 source_start, TRange1 source_end, TRange2 output_start, TRange3 output_end)
     {
-        return output_start + (output_end - output_start) * ((value - source_start) / (source_end - source_start));
+        const auto val = static_cast<TIntermediate>(value);
+        const auto src_st = static_cast<TIntermediate>(source_start);
+        const auto src_en = static_cast<TIntermediate>(source_end);
+        const auto out_st = static_cast<TIntermediate>(output_start);
+        const auto out_en = static_cast<TIntermediate>(output_end);
+
+        return out_st + (out_en - out_st) * ((val - src_st) / (src_en - src_st));
     }
 } // namespace ien
