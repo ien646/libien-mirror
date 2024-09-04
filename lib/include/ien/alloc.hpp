327d71c60f4c8265273b13fbb172ab4f361518f6
ien646 <1340246+ien646@users.noreply.github.com>
2024-09-04 00:00:00 +0000

    roomba

--- lib/include/ien/alloc.hpp
+++ lib/include/ien/alloc.hpp
@@ -80,9 +80,9 @@ namespace ien
         using difference_type = std::ptrdiff_t;
         using propagate_on_container_move_assignment = std::true_type;
 
-        constexpr T* allocate(size_t n) { return aligned_alloc<T>(n, Alignment); }
+        constexpr T* allocate(size_t n) const { return aligned_alloc<T>(n, Alignment); }
 
-        void deallocate(T* ptr, [[maybe_unused]] size_t n) { return aligned_free(ptr); }
+        void deallocate(T* ptr, [[maybe_unused]] size_t n) const { return aligned_free(ptr); }
 
         // Removed in c++20 but some compilers require it
         template<typename U>
